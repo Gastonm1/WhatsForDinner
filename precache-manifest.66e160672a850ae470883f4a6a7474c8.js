f528bc4079c8bef9ab79ab6ebcf073964f987a47
Gastonm1 <gaston.marques@gmail.com>
2020-11-18 23:02:33 -0600

    Updates

---
+++ precache-manifest.66e160672a850ae470883f4a6a7474c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "165818fd9b8e39838e1be2c96367aeea",
+    "url": "/WhatsForDinner/index.html"
+  },
+  {
+    "revision": "4e1c74addbbd60e4f68e",
+    "url": "/WhatsForDinner/static/css/main.c0a252a0.chunk.css"
+  },
+  {
+    "revision": "7e68185431ebe3504f25",
+    "url": "/WhatsForDinner/static/js/2.224214a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WhatsForDinner/static/js/2.224214a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e1c74addbbd60e4f68e",
+    "url": "/WhatsForDinner/static/js/main.b3022b68.chunk.js"
+  },
+  {
+    "revision": "c16717e2cdf1c3bd2772",
+    "url": "/WhatsForDinner/static/js/runtime-main.b7fe4658.js"
+  }
+]);
